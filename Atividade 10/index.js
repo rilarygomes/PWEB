455df6a5f05817d5ba52ec3828a0203e4a33da9a
Rilary Gomes <70041774+rilarygomes@users.noreply.github.com>
2022-05-04 21:52:03 -0300

    Add files via upload

---
+++ Atividade 10/index.js
@@ -0,0 +1,18 @@
+var aluno1 = new Object()
+aluno1.nome = "Rilary";
+aluno1.ra = "123";
+
+alert(JSON.stringify(aluno1));
+
+var aluno2 = {}
+aluno2.nome = "Carlos";
+aluno2.ra = "321";
+
+alert(JSON.stringify(aluno2));
+
+var aluno3 = {
+    nome: "Harry",
+    ra: "456"
+};
+
+alert(JSON.stringify(aluno3));
